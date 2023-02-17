39924a8624b93f3e9cdf8b415a5adf7d7dc6f368
Sebastiano Tocci <seba.tocci@gmail.com>
2023-02-17 15:57:36 +0100

    ported everything to js

--- dist/middleware/swagger.ui.options.js
+++ dist/middleware/swagger.ui.options.js
@@ -1,23 +1,21 @@
-export class SwaggerUiOptions {
-    public apiDocsPath: string;
-    public swaggerUIPath: string;
-    public swaggerUiDir: string;
-
-    constructor(apiDocsPath: string, swaggerUIPath: string, swaggerUiDir: string) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SwaggerUiOptions = void 0;
+class SwaggerUiOptions {
+    constructor(apiDocsPath, swaggerUIPath, swaggerUiDir) {
         this.apiDocsPath = apiDocsPath;
         this.swaggerUIPath = swaggerUIPath;
         this.swaggerUiDir = swaggerUiDir;
-
         this.sanitize();
     }
-
-    private sanitize() {
+    sanitize() {
         if (this.apiDocsPath.charAt(this.apiDocsPath.length - 1) === '/') {
             this.apiDocsPath = this.apiDocsPath.substring(0, this.apiDocsPath.length - 1);
         }
-
         if (this.swaggerUIPath.charAt(this.swaggerUIPath.length - 1) === '/') {
             this.swaggerUIPath = this.swaggerUIPath.substring(0, this.swaggerUIPath.length - 1);
         }
     }
 }
+exports.SwaggerUiOptions = SwaggerUiOptions;
+//# sourceMappingURL=swagger.ui.options.js.map
